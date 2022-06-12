0a51fed5e7c59b724f1544857557904f07f6ef6d
Rafael Santana <rafaelns.br@gmail.com>
2022-06-11 21:43:55 -0300

    Criando novas operações e lançando erro ao não reconhecer operação

--- operacoes.py
+++ operacoes.py
@@ -9,3 +9,12 @@ def multiplicacao(a, b):
 
 def divisao(a, b):
     return a / b
+
+def potencia(a, b):
+    if b == 0:
+        return 1
+    
+    return a * potencia(a, b - 1)
+
+def resto(a, b):
+    return a % b
