c2fa5e6bb739b72fe74c9e8347f10cb44149b6c1
Rafael Santana <rafaelns.br@gmail.com>
2022-06-11 21:36:44 -0300

    Reordenando inputs e aceitando símbolos como a operação

--- calculadora.py
+++ calculadora.py
@@ -1,20 +1,20 @@
 import operacoes
 
-operacao = input('Entre com a operação: ')
-
 primeiro_numero = int(input('Entre com o primeiro número: '))
+
+operacao = input('Entre com a operação: ')
 
 segundo_numero = int(input('Entre com o segundo número: '))
 
 resultado = None
 
-if operacao == 'soma':
+if operacao == 'soma' or operacao == '+':
     resultado = operacoes.soma(primeiro_numero, segundo_numero)
-if operacao == 'subtração':
+if operacao == 'subtração' or operacao == '-':
     resultado = operacoes.subtracao(primeiro_numero, segundo_numero)
-if operacao == 'multiplicação':
+if operacao == 'multiplicação' or operacao == '*':
     resultado = operacoes.multiplicacao(primeiro_numero, segundo_numero)
-if operacao == 'divisão':
+if operacao == 'divisão' or operacao == '/':
     resultado = operacoes.divisao(primeiro_numero, segundo_numero)
 
 print('Resultado:', resultado)
