bd7da68fffcf8e3e3d366bfb54cb8df12b16ae50
Rafael Santana <rafaelns.br@gmail.com>
2023-07-16 23:49:24 -0300

    Adicionando operações de potência e resto e lançando exceção em caso de operação desconhecida

--- src/calculadora.py
+++ src/calculadora.py
@@ -10,11 +10,17 @@ resultado = None
 
 if operacao == 'soma':
     resultado = operacoes.soma(primeiro_numero, segundo_numero)
-if operacao == 'subtração':
+elif operacao == 'subtração':
     resultado = operacoes.subtracao(primeiro_numero, segundo_numero)
-if operacao == 'multiplicação':
+elif operacao == 'multiplicação':
     resultado = operacoes.multiplicacao(primeiro_numero, segundo_numero)
-if operacao == 'divisão':
+elif operacao == 'divisão':
     resultado = operacoes.divisao(primeiro_numero, segundo_numero)
+elif operacao == 'potência':
+    resultado = operacoes.potencia(primeiro_numero, segundo_numero)
+elif operacao == 'resto':
+    resultado = operacoes.resto(primeiro_numero, segundo_numero)
+else:
+    raise Exception(f"Operação '{operacao}' não reconhecida!")
 
 print('Resultado:', resultado)
